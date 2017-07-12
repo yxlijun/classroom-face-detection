48cbcf90b37eb6d62dd6a3afc31bff4ab2ae2c27
lijun <2644089822@qq.com>
2017-07-12 15:32:36 +0800

    完成教室人脸检测项目

---
+++ mini-caffe/src/layers/sigmoid_layer.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <vector>
+
+#include "./sigmoid_layer.hpp"
+
+#ifdef USE_CUDNN
+#include "./cudnn/cudnn_sigmoid_layer.hpp"
+#endif  // USE_CUDNN
+
+namespace caffe {
+
+inline real_t sigmoid(real_t x) {
+  return 1. / (1. + exp(-x));
+}
+
+void SigmoidLayer::Forward_cpu(const vector<Blob*>& bottom,
+                               const vector<Blob*>& top) {
+  const real_t* bottom_data = bottom[0]->cpu_data();
+  real_t* top_data = top[0]->mutable_cpu_data();
+  const int count = bottom[0]->count();
+  for (int i = 0; i < count; ++i) {
+    top_data[i] = sigmoid(bottom_data[i]);
+  }
+}
+
+#ifndef USE_CUDA
+STUB_GPU(SigmoidLayer);
+#endif
+
+// Creator
+
+static shared_ptr<Layer> CreateLayer(const LayerParameter& param) {
+#ifdef USE_CUDNN
+  return shared_ptr<Layer>(new CuDNNSigmoidLayer(param));
+#else
+  return shared_ptr<Layer>(new SigmoidLayer(param));
+#endif  // USE_CUDNN
+}
+
+REGISTER_LAYER_CREATOR(Sigmoid, CreateLayer);
+
+}  // namespace caffe
---
+++ mini-caffe/src/syncedmem.cpp
@@ -0,0 +1,141 @@
+#include "./common.hpp"
+#include "./syncedmem.hpp"
+#include "./util/math_functions.hpp"
+
+namespace caffe {
+
+SyncedMemory::~SyncedMemory() {
+  if (cpu_ptr_ && own_cpu_data_) {
+    CaffeFreeHost(cpu_ptr_, cpu_malloc_use_cuda_);
+  }
+
+#ifdef USE_CUDA
+  if (gpu_ptr_ && own_gpu_data_) {
+    int initial_device;
+    cudaGetDevice(&initial_device);
+    if (gpu_device_ != -1) {
+      CUDA_CHECK(cudaSetDevice(gpu_device_));
+    }
+    CUDA_CHECK(cudaFree(gpu_ptr_));
+    cudaSetDevice(initial_device);
+  }
+#endif  // USE_CUDA
+}
+
+inline void SyncedMemory::to_cpu() {
+  switch (head_) {
+  case UNINITIALIZED:
+    CaffeMallocHost(&cpu_ptr_, size_, &cpu_malloc_use_cuda_);
+    caffe_memset(size_, 0, cpu_ptr_);
+    head_ = HEAD_AT_CPU;
+    own_cpu_data_ = true;
+    break;
+  case HEAD_AT_GPU:
+#ifdef USE_CUDA
+    if (cpu_ptr_ == NULL) {
+      CaffeMallocHost(&cpu_ptr_, size_, &cpu_malloc_use_cuda_);
+      own_cpu_data_ = true;
+    }
+    caffe_gpu_memcpy(size_, gpu_ptr_, cpu_ptr_);
+    head_ = SYNCED;
+#else
+    NO_GPU;
+#endif  // USE_CUDA
+    break;
+  case HEAD_AT_CPU:
+  case SYNCED:
+    break;
+  }
+}
+
+inline void SyncedMemory::to_gpu() {
+#ifdef USE_CUDA
+  switch (head_) {
+  case UNINITIALIZED:
+    CUDA_CHECK(cudaGetDevice(&gpu_device_));
+    CUDA_CHECK(cudaMalloc(&gpu_ptr_, size_));
+    caffe_gpu_memset(size_, 0, gpu_ptr_);
+    head_ = HEAD_AT_GPU;
+    own_gpu_data_ = true;
+    break;
+  case HEAD_AT_CPU:
+    if (gpu_ptr_ == NULL) {
+      CUDA_CHECK(cudaGetDevice(&gpu_device_));
+      CUDA_CHECK(cudaMalloc(&gpu_ptr_, size_));
+      own_gpu_data_ = true;
+    }
+    caffe_gpu_memcpy(size_, cpu_ptr_, gpu_ptr_);
+    head_ = SYNCED;
+    break;
+  case HEAD_AT_GPU:
+  case SYNCED:
+    break;
+  }
+#else
+  NO_GPU;
+#endif  // USE_CUDA
+}
+
+const void* SyncedMemory::cpu_data() {
+  to_cpu();
+  return (const void*)cpu_ptr_;
+}
+
+void SyncedMemory::set_cpu_data(void* data) {
+  CHECK(data);
+  if (own_cpu_data_) {
+    CaffeFreeHost(cpu_ptr_, cpu_malloc_use_cuda_);
+  }
+  cpu_ptr_ = data;
+  head_ = HEAD_AT_CPU;
+  own_cpu_data_ = false;
+}
+
+const void* SyncedMemory::gpu_data() {
+#ifdef USE_CUDA
+  to_gpu();
+  return (const void*)gpu_ptr_;
+#else
+  NO_GPU;
+  return NULL;
+#endif  // USE_CUDA
+}
+
+void SyncedMemory::set_gpu_data(void* data) {
+#ifdef USE_CUDA
+  CHECK(data);
+  if (own_gpu_data_) {
+    int initial_device;
+    cudaGetDevice(&initial_device);
+    if (gpu_device_ != -1) {
+      CUDA_CHECK(cudaSetDevice(gpu_device_));
+    }
+    CUDA_CHECK(cudaFree(gpu_ptr_));
+    cudaSetDevice(initial_device);
+  }
+  gpu_ptr_ = data;
+  head_ = HEAD_AT_GPU;
+  own_gpu_data_ = false;
+#else
+  NO_GPU;
+#endif  // USE_CUDA
+}
+
+void* SyncedMemory::mutable_cpu_data() {
+  to_cpu();
+  head_ = HEAD_AT_CPU;
+  return cpu_ptr_;
+}
+
+void* SyncedMemory::mutable_gpu_data() {
+#ifdef USE_CUDA
+  to_gpu();
+  head_ = HEAD_AT_GPU;
+  return gpu_ptr_;
+#else
+  NO_GPU;
+  return NULL;
+#endif  // USE_CUDA
+}
+
+}  // namespace caffe
